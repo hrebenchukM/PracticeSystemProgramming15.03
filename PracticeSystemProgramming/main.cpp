4c8a5f7dd444eb6f60308acb0e909104a28408e7
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2024-06-09 03:08:53 +0300

    //

--- PracticeSystemProgramming/main.cpp
+++ PracticeSystemProgramming/main.cpp
@@ -5,7 +5,7 @@
 
 int WINAPI _tWinMain(HINSTANCE hInst, HINSTANCE hPrev, LPTSTR lpszCmdLine, int nCmdShow)
 {
-    system("chcp 1251");
+
     WordSearcher dlg;
     return DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), NULL, WordSearcher::DlgProc);
 }
